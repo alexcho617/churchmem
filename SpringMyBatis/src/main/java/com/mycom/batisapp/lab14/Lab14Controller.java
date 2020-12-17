a13f1ec5cc511711495b05b333b2bbd5ec757f9c
alexcho617 <38528052+alexcho617@users.noreply.github.com>
2020-12-17 17:45:18 +0900

    fixed typo

--- SpringMyBatis/src/main/java/com/mycom/batisapp/lab14/Lab14Controller.java
+++ SpringMyBatis/src/main/java/com/mycom/batisapp/lab14/Lab14Controller.java
@@ -1,4 +1,4 @@
-`package com.mycom.batisapp.lab14;
+package com.mycom.batisapp.lab14;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
